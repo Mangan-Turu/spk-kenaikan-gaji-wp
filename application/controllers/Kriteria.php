207c27e9e6e70d173be1dcb2b2ec5f36b2dbeb63
In'am Falahuddin <inamfalahuddin06@gmail.com>
2025-05-24 00:26:09 +0700

    update db

--- application/controllers/Kriteria.php
+++ application/controllers/Kriteria.php
@@ -8,11 +8,14 @@ class Kriteria extends CI_Controller
         parent::__construct();
         $this->load->library('form_validation');
         $this->load->model('Kriteria_model');
+        $this->load->model('Kriteria_sub_model');
     }
 
     public function index()
     {
         $data['kriteria'] = $this->Kriteria_model->get_all();
+        $data['kriteria_sub'] = $this->Kriteria_sub_model->get_all();
+        echo $data['kriteria_sub'];
         $data['contents'] = $this->load->view('kriteria_view', $data, true);
         $this->load->view('templates/admin_templates', $data);
     }
--- application/models/Kriteria_sub_model.php
+++ application/models/Kriteria_sub_model.php
@@ -5,23 +5,66 @@ class Kriteria_sub_model extends CI_Model
 {
     private $table = 'kriteria_sub';
 
+    protected $user_id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->user_id = $this->session->userdata('user_id');
+    }
+
     public function insert($data)
     {
+        $data['created_by'] = $this->user_id;
+        $data['updated_by'] = $this->user_id;
         return $this->db->insert($this->table, $data);
     }
 
     public function update($id, $data)
     {
-        return $this->db->update($this->table, $data, ['id' => $id]);
+        $data['updated_by'] = $this->user_id;
+        return $this->db->update($this->table, $data, [
+            'id'         => $id,
+            'created_by' => $this->user_id
+        ]);
     }
 
     public function delete($id)
     {
-        return $this->db->delete($this->table, ['id' => $id]);
+        return $this->db->delete($this->table, [
+            'id' => $id,
+            'created_by' => $this->user_id
+        ]);
     }
 
     public function get_all()
     {
-        return $this->db->get($this->table)->result();
+        $this->db->select('
+            kriteria_sub.id,
+            kriteria.id AS kriteria_id,
+            kriteria.kode,
+            kriteria.nama,
+            kriteria_sub.nilai,
+            kriteria_sub.deskripsi,
+            kriteria_sub.created_at,
+            kriteria_sub.updated_at,
+            kriteria_sub.created_by,
+            kriteria_sub.updated_by
+        ');
+        $this->db->from('kriteria_sub');
+        $this->db->join('kriteria', 'kriteria.id = kriteria_sub.kriteria_id');
+        $this->db->where('kriteria_sub.created_by', $this->user_id);
+
+        $query = $this->db->get();
+        return $query->result();
+    }
+
+    public function get_by_id($id)
+    {
+        return $this->db
+            ->where('id', $id)
+            ->where('created_by', $this->user_id)
+            ->get($this->table)
+            ->row();
     }
 }
